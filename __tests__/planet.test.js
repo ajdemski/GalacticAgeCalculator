73f7ba4049a18be3c33dcb0bcfe6c142a0bedf95
Aaron Demski <ajdemski@frontier.com>
2023-12-23 16:11:29 -0800

    good fail

--- __tests__/planet.test.js
+++ __tests__/planet.test.js
@@ -1,12 +1,22 @@
-import AgeCalculator from '../src/js/ageCalculator.js';
+import AgeCalculator from "../src/js/ageCalculator.js";
 
-describe('AgeCalculator', () => {
+describe("AgeCalculator", () => {
+  test("should correctly instantiate AgeCalculator with name and age", () => {
+    const ageCalculator = new AgeCalculator("Aaron", 30);
 
-  test('should correctly instantiate AgeCalculator with name and age', () => {
-    const ageCalculator = new AgeCalculator('Aaron', 30);
     expect(ageCalculator).toBeInstanceOf(AgeCalculator);
     expect(ageCalculator.name).toBe("Aaron");
     expect(ageCalculator.age).toBe(30);
   });
 
+  test("should correctly calculate the years passed on each planet based off the user age input", () => {
+    const ageCalculator = new AgeCalculator("Aaron", 56);
+    const yearsPassedResult = ageCalculator.yearsPassedSinceBirthday(43);
+
+    expect(yearsPassedResult.earth).toBe(13);
+    expect(yearsPassedResult.mercury).toBeCloseTo(54.16, 2);
+    expect(yearsPassedResult.venus).toBeCloseTo(20.96, 2);
+    expect(yearsPassedResult.mars).toBeCloseTo(6.91, 2);
+    expect(yearsPassedResult.jupiter).toBeCloseTo(1.09, 2);
+  });
 });
--- src/js/ageCalculator.js
+++ src/js/ageCalculator.js
@@ -3,4 +3,17 @@ export default class AgeCalculator {
     this.name = name;
     this.age = age;
   }
+
+  yearsPassedSinceBirthday(pastAge) {
+    const yearsPassed = this.age - pastAge;
+    return {
+      earth: yearsPassed,
+      mercury: yearsPassed * 0.24,
+      venus: yearsPassed * 0.62,
+      mars: yearsPassed * 1.88,
+      jupiter: yearsPassed * 11.86,
+    };
+  }
+
 }
+
